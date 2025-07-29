c32187a334b3f7eddeb0d29a444c4e51ed82ca64
arfat-xyz <arfatrahman08@gmail.com>
2025-07-30 00:33:42 +0600

    footer change 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 import "./globals.css";
-import { Footer } from "@/components/footer";
 const baseUrl =
   process.env.NEXT_PUBLIC_SITE_URL || "https://md-board.vercel.app";
 export const metadata: Metadata = {
